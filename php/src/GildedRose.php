ff3d030ef844f1b07f11faef87e7763e7aa51b76
Max Humme <max@maxhumme.nl>
2024-05-19 17:18:23 +0200

    Simplify GildedRose class a bit

--- php/src/GildedRose.php
+++ php/src/GildedRose.php
@@ -10,55 +10,53 @@ final class GildedRose
      * @param Item[] $items
      */
     public function __construct(
-        private array $items
+        private readonly array $items
     ) {
     }
 
     public function updateQuality(): void
     {
+        // loop items
         foreach ($this->items as $item) {
-            if ($item->name !== 'Aged Brie' and $item->name !== 'Backstage passes to a TAFKAL80ETC concert') {
-                if ($item->quality > 0) {
-                    if ($item->name !== 'Sulfuras, Hand of Ragnaros') {
-                        $item->quality = $item->quality - 1;
-                    }
-                }
-            } else {
-                if ($item->quality < 50) {
-                    $item->quality = $item->quality + 1;
+            // handle quality when sell date is not passed
+            if ($item->name === 'Aged Brie' || $item->name === 'Backstage passes to a TAFKAL80ETC concert') {
+                if ($item->quality < 50) { // spec: quality can never increase above 50
+                    $item->quality++; // spec: aged brie and backstage passes increase in quality the older they get
                     if ($item->name === 'Backstage passes to a TAFKAL80ETC concert') {
                         if ($item->sellIn < 11) {
                             if ($item->quality < 50) {
-                                $item->quality = $item->quality + 1;
+                                $item->quality = $item->quality + 1; // spec backstage passes increase by 2 when sellIn is < 11
                             }
                         }
                         if ($item->sellIn < 6) {
                             if ($item->quality < 50) {
-                                $item->quality = $item->quality + 1;
+                                $item->quality = $item->quality + 1; // spec backstage passes increase by 2 when sellIn is < 6
                             }
                         }
                     }
                 }
+            } elseif ($item->quality > 0) { // spec: quality is never negative
+                if ($item->name !== 'Sulfuras, Hand of Ragnaros') { // spec: sulfuras never decreases in quality
+                    $item->quality--; // spec: standard items' quality decrease by 1 every day
+                }
             }
 
-            if ($item->name !== 'Sulfuras, Hand of Ragnaros') {
-                $item->sellIn = $item->sellIn - 1;
+            // subtract a day from sellIn
+            if ($item->name !== 'Sulfuras, Hand of Ragnaros') { // spec: sulfuras never has to be sold
+                $item->sellIn--;
             }
 
+            // handle 'passed sell date' quality rules
             if ($item->sellIn < 0) {
-                if ($item->name !== 'Aged Brie') {
-                    if ($item->name !== 'Backstage passes to a TAFKAL80ETC concert') {
-                        if ($item->quality > 0) {
-                            if ($item->name !== 'Sulfuras, Hand of Ragnaros') {
-                                $item->quality = $item->quality - 1;
-                            }
-                        }
-                    } else {
-                        $item->quality = $item->quality - $item->quality;
-                    }
-                } else {
+                if ($item->name === 'Aged Brie') {
                     if ($item->quality < 50) {
-                        $item->quality = $item->quality + 1;
+                        $item->quality++; // spec: quality of aged brie increases the older it gets
+                    }
+                } elseif ($item->name === 'Backstage passes to a TAFKAL80ETC concert') {
+                    $item->quality = 0; // spec: backstage passes are worthless after the concert
+                } elseif ($item->quality > 0) { // spec: quality is never negative
+                    if ($item->name !== 'Sulfuras, Hand of Ragnaros') { // spec: sulfuras never decreases in quality
+                        $item->quality--; // spec: once the sellIn date has passed, quality degrades twice as fast
                     }
                 }
             }
