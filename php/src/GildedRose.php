6c9802a95e6dc10ea4ba82b77a1993dc17f14301
Max Humme <max@maxhumme.nl>
2024-05-19 17:47:56 +0200

    Group logic per item

--- php/src/GildedRose.php
+++ php/src/GildedRose.php
@@ -25,42 +25,48 @@ final class GildedRose
                 continue; // spec: sulfuras never has to be sold and never decreases in quality
             }
 
-            // handle quality when sell date is not passed
-            if ($item->name === 'Aged Brie' || $item->name === 'Backstage passes to a TAFKAL80ETC concert') {
+            if ($item->name === 'Aged Brie') {
                 if ($item->quality < self::QUALITY_MAX) { // spec: quality can never increase above self::QUALITY_MAX
                     $item->quality++; // spec: aged brie and backstage passes increase in quality the older they get
-                    if ($item->name === 'Backstage passes to a TAFKAL80ETC concert') {
-                        if ($item->sellIn < 11) {
-                            if ($item->quality < self::QUALITY_MAX) { // spec: quality can never increase above self::QUALITY_MAX
-                                $item->quality++; // spec backstage passes increase by 2 when sellIn is < 11
-                            }
+                }
+                if ($item->sellIn <= 0) {
+                    if ($item->quality < self::QUALITY_MAX) {
+                        $item->quality++; // spec: quality of aged brie increases the older it gets
+                    }
+                }
+            } elseif ($item->name === 'Backstage passes to a TAFKAL80ETC concert') {
+                if ($item->quality < self::QUALITY_MAX) { // spec: quality can never increase above self::QUALITY_MAX
+                    $item->quality++; // spec: aged brie and backstage passes increase in quality the older they get
+                    if ($item->sellIn < 11) {
+                        if ($item->quality < self::QUALITY_MAX) { // spec: quality can never increase above self::QUALITY_MAX
+                            $item->quality++; // spec backstage passes increase by 2 when sellIn is < 11
                         }
-                        if ($item->sellIn < 6) {
-                            if ($item->quality < self::QUALITY_MAX) { // spec: quality can never increase above self::QUALITY_MAX
-                                $item->quality++; // spec backstage passes increase by 3 when sellIn is < 6
-                            }
+                    }
+                    if ($item->sellIn < 6) {
+                        if ($item->quality < self::QUALITY_MAX) { // spec: quality can never increase above self::QUALITY_MAX
+                            $item->quality++; // spec backstage passes increase by 3 when sellIn is < 6
                         }
                     }
                 }
-            } elseif ($item->quality > self::QUALITY_MIN) { // spec: quality is never negative
-                $item->quality--;
+                if ($item->sellIn <= 0) {
+                    $item->quality = self::QUALITY_MIN; // spec: backstage passes are worthless after the concert
+                }
+            } else {
+                // handle quality when sell date is not passed
+                if ($item->quality > self::QUALITY_MIN) { // spec: quality is never negative
+                    $item->quality--;
+                }
+
+                // handle 'passed sell date' quality rules
+                if ($item->sellIn <= 0) {
+                    if ($item->quality > self::QUALITY_MIN) { // spec: quality is never negative
+                        $item->quality--; // spec: once the sellIn date has passed, quality degrades twice as fast
+                    }
+                }
             }
 
             // subtract a day from sellIn
             $item->sellIn--;
-
-            // handle 'passed sell date' quality rules
-            if ($item->sellIn < 0) {
-                if ($item->name === 'Aged Brie') {
-                    if ($item->quality < self::QUALITY_MAX) {
-                        $item->quality++; // spec: quality of aged brie increases the older it gets
-                    }
-                } elseif ($item->name === 'Backstage passes to a TAFKAL80ETC concert') {
-                    $item->quality = self::QUALITY_MIN; // spec: backstage passes are worthless after the concert
-                } elseif ($item->quality > self::QUALITY_MIN) { // spec: quality is never negative
-                    $item->quality--; // spec: once the sellIn date has passed, quality degrades twice as fast
-                }
-            }
         }
     }
 }
